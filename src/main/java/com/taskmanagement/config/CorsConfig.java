5b57789c18c84ff29152dad2a554c14f978e5bfe
vvivek2 <vaibhavvivek1704@gmail.com>
2026-02-25 18:56:23 +0000

    fixed cors issue

--- src/main/java/com/taskmanagement/config/CorsConfig.java
+++ src/main/java/com/taskmanagement/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "https://task-management-api-s2m8.onrender.com/"
+                                "https://task-management-turbo-ui.onrender.com/"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
